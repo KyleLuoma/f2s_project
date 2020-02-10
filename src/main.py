9c497b2a75f0e5d70a3556e51bc69b77a94052c0
kyle.r.luoma <kyle.r.luoma.mil@mail.mil>
2020-02-10 14:13:13 -0500

    added match stub and calls to data load functions

--- src/main.py
+++ src/main.py
@@ -11,6 +11,22 @@ import load_data
 import utility
 
 def main():
+    print("You need to write some code here")
+    criteria = pd.Series(data = ["PARA", "LN"])
+    
+    drrsa = load_drrsa_file()
+    acom_spaces = load_army_command_aos_billets()
+    faces = load_emilpo()
+    
+"""
+" Core matching function that iterates through available spaces and aligns
+" faces based on matching criteria passed in the criteria list
+" Parameters: Pandas series of matching criteria, 
+              Pandas DF of available spaces, 
+              Pandas DF of available faces
+"""
+def match(criteria, spaces, faces):
+    
     
 
 if (__name__ == "__main__"): main()
