65fdbdc619d0987c866536408b3d4224a55d455d
kyle.r.luoma <kyle.r.luoma.mil@mail.mil>
2020-02-11 17:33:43 -0500

    Added data processing tasks to consolidate ASI, SQI and MOS_AOC into lists

--- src/process_data.py
+++ src/process_data.py
@@ -23,4 +23,69 @@ def process_emilpo_assignments(emilpo_assignments, rank_grade_xwalk):
     emilpo_assignments["GRADE"] = emilpo_assignments.apply(
             lambda row: rank_grade_xwalk.loc[row.RANK_AB].GRADE, axis = 1
             )
+    
+    print("Consolidating ASIs into ASI_LIST column in the eMILPO assignments file")
+    emilpo_assignments["ASI_LIST"] = emilpo_assignments.apply(
+            lambda row: [
+                    row.ASI1,
+                    row.ASI2,
+                    row.ASI3,
+                    row.ASI4,
+                    row.ASI5,
+                    row.ASI6,
+                    row.ASI7,
+                    row.ASI8,
+                    row.ASI9,
+                    row.ASI10,
+                    row.ASI11,
+                    row.ASI12,
+                    row.ASI13,
+                    row.ASI14
+                    ],
+                    axis = 1
+            )
+    
+    print("Consolidating SQIs into SQI_LIST column in the eMILPO assignments file")
+    emilpo_assignments["SQI_LIST"] = emilpo_assignments.apply(
+            lambda row: [
+                    row.SQI1,
+                    row.SQI2,
+                    row.SQI3,
+                    row.SQI4,
+                    row.SQI5,
+                    row.SQI6,
+                    row.SQI7,
+                    row.SQI8,
+                    row.SQI9,
+                    row.SQI10,
+                    row.SQI11,
+                    row.SQI12,
+                    row.SQI13,
+                    row.SQI14,
+                    row.SQI15,
+                    row.SQI16
+                    ],
+                    axis = 1
+            )
+    
+    print("Consolidating MOS/AOC into MOS_AOC_LIST column in the eMILPO assignments file")
+    emilpo_assignments["MOS_AOC_LIST"] = emilpo_assignments.apply(
+            lambda row: [
+                    row.MOS_AOC1,
+                    row.MOS_AOC2,
+                    row.MOS_AOC3,
+                    row.MOS_AOC4,
+                    row.MOS_AOC5,
+                    row.MOS_AOC6,
+                    row.MOS_AOC7,
+                    row.MOS_AOC8,
+                    row.MOS_AOC9,
+                    row.MOS_AOC10,
+                    row.MOS_AOC11,
+                    row.MOS_AOC12,
+                    row.MOS_AOC13
+                    ],
+                    axis = 1
+            )
+    
     return emilpo_assignments
