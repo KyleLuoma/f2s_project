56ac443999295325e723386216c0a3ab7bbf7ccb
kyle.r.luoma <kyle.r.luoma.mil@mail.mil>
2020-11-17 09:24:57 -0500

    Updated file dates

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ LOAD_MATCH_PHASES = True
 LOAD_AND_PROCESS_MAPS = True
 LOAD_COMMAND_CONSIDERATIONS = True
 PROCESS_COMMAND_CONSIDERATIONS = True
-LOAD_AND_PROCESS_SPACES = False
+LOAD_AND_PROCESS_SPACES = True
 LOAD_AND_PROCESS_ADDRESS_DATA = True
 LOAD_EMILPO_FACES = True
 LOAD_EMILPO_TEMP_ASSIGNMENTS = True
@@ -31,12 +31,12 @@ LOAD_RCMS_FACES = True
 VERBOSE = False
 RUN_MATCH = True
 EXPORT_F2S = True
-GENERATE_CMD_METRICS = False
+GENERATE_CMD_METRICS = True
 EXPORT_UNMATCHED = False
-EXPORT_UNMASKED = False #Export ONLY to your local drive, not to a network folder
+EXPORT_UNMASKED = True #Export ONLY to your local drive, not to a network folder
 UPDATE_CONNECTIONS = True
 EXPORT_CMD_SPECS = True
-COMMAND_EXPORT_LIST = ["AR"] #Leave empty to export all commands
+COMMAND_EXPORT_LIST = [] #Leave empty to export all commands
 
 
 def main():
